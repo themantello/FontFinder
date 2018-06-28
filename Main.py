455ad85359ca26553fd3e6b5cbcade64b1e1e95e
themantello <thechoy@gmail.com>
2018-06-28 16:23:12 -0400

    Modified countfiles to check MIME types, changed xml parser method to actually work

--- Main.py
+++ Main.py
@@ -1,48 +1,45 @@
 import os
 import zipfile
 import xml.etree.cElementTree as ET
+import mimetypes
+
+
+font_list = []
+
 
 def main():
     countfiles()
-    #   extractZip()
-    #   parseXML()
+    print(font_list)
 
 
 def countfiles():
-    docx_counter = 0
-    pptx_counter = 0
     for root, dirs, files in os.walk('Test folder'):
         for file in files:
-            print(file)
-            if str.lower(file).endswith('.docx'):
-                docx_counter += 1
-            if str.lower(file).endswith('.pptx'):
-                pptx_counter += 1
-
-    print('.docx counter is ' + str(docx_counter))
-    print('.pptx counter is ' + str(pptx_counter))
+            fileType = mimetypes.guess_type(file)[0]
+            if str(fileType).startswith('application/vnd.openxml'):
+                parsexml((extractzip(os.path.join(root, file))))
     return
 
 
-def extractZip():
-    my_zip = zipfile.ZipFile('Test Folder/Fonts.docx', 'r')
+def extractzip(xfile):
+    my_zip = zipfile.ZipFile(xfile, 'r')
     with my_zip as zip_file:
         for member in zip_file.namelist():
             filename = os.path.basename(member)
             if filename == 'document.xml':
-                source = zip_file.open(member)
-                zip_file.extract(member)
-
-
+                source = zip_file.read(member)
     my_zip.close()
+    return source
+
+
+def parsexml(sourcexml):
+    tree = ET.ElementTree(ET.fromstring(sourcexml))
+    root = tree.getroot()
+    namespaces = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+    for font in root.findall("w:body/w:p/w:pPr/w:rPr/w:rFonts", namespaces):
+        font_list.append((font.get("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}ascii")))
     return
 
-def parseXML():
-    tree = ET.parse('word/document.xml')
-    root = tree.getroot()
-    for child in root:
-        print(child.tag, child.attrib)
-    return
 
 if __name__ == "__main__":
     main()
