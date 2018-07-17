232d9b90182774979ecc2dd5341ffe197291f8f1
twilcox <thechoy@gmail.com>
2018-07-17 15:57:56 -0400

    Completed sorted output of fonts.

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import xml.etree.cElementTree as ET
 import mimetypes
+import collections
 
 
 font_list = []
@@ -10,13 +11,20 @@ font_list = []
 def main():
     countfiles()
     print(font_list)
+    counts = collections.Counter(font_list)
+    sortedfonts = sorted(font_list, key=counts.get, reverse=True)
+    seen = []
     outfile = open("UsedFonts.txt", "w")
-    for font in font_list:
-        outfile.write(font + '\n')
+    for font in sortedfonts:
+        if font not in seen:
+            seen.append(font)
+            outfile.write(font + '\n')
     outfile.close()
 
+
 def countfiles():
-    for root, dirs, files in os.walk('Test folder'):
+    """Walk through current directory and find office documents to process"""
+    for root, dirs, files in os.walk('.'):
         for file in files:
             fileType = mimetypes.guess_type(file)[0]
             if str(fileType).startswith('application/vnd.openxml'):
@@ -25,6 +33,7 @@ def countfiles():
 
 
 def extractzip(xfile):
+    """Extracts the fontTable.xml file from the docx archive for processing"""
     my_zip = zipfile.ZipFile(xfile, 'r')
     with my_zip as zip_file:
         for member in zip_file.namelist():
@@ -37,6 +46,7 @@ def extractzip(xfile):
 
 
 def parsexml(sourcexml):
+    """Searches the XML for font tags, specifically w:font"""
     tree = ET.ElementTree(ET.fromstring(sourcexml))
     root = tree.getroot()
     namespaces = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
